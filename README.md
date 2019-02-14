4ba0c44a0c363faae75df53f34749b1d2ad04c5f
Ghafran Abbas <ghafran@Ghafrans-MBP-2.fios-router.home>
2019-02-14 09:36:10 -0500

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+export GITLAB_URL=
+export GITLAB_TOKEN=
+git clone https://github.com/ghafran/gitlab-snippets-backup.git
+cd gitlab-snippets-backup
+node private.js
+node public.js
+```
---
+++ public.js
@@ -0,0 +1,52 @@
+var _ = require('lodash'),
+    fs = require('fs'),
+    promise = require('bluebird'),
+    rp = require('request-promise');
+
+var url = process.env.GITLAB_URL;
+
+if (!url) {
+    console.error('GITLAB_URL environment variable not set');
+    process.exit();
+}
+
+var token = process.env.GITLAB_TOKEN;
+
+if (!token) {
+    console.error('GITLAB_TOKEN environment variable not set');
+    process.exit();
+}
+
+if (!fs.existsSync(`${__dirname}/snippets`)) {
+    fs.mkdirSync(`${__dirname}/snippets`);
+}
+
+var dir = `${__dirname}/snippets/public`;
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+rp({
+    method: 'GET',
+    uri: `${url}/api/v4/snippets/public`,
+    headers: {
+        'PRIVATE-TOKEN': token
+    },
+    json: true
+}).then((snippets) => {
+    fs.writeFileSync(`${dir}/snippets.json`, JSON.stringify(snippets));
+    return promise.mapSeries(snippets, (snippet) => {
+        var filename = `${snippet.id}-${snippet.title}`;
+        return rp({
+            method: 'GET',
+            uri: `${url}/snippets/${snippet.id}/raw`,
+            headers: {
+                'PRIVATE-TOKEN': token
+            }
+        }).then((raw) => {
+            fs.writeFileSync(`${dir}/${filename}`, raw);
+        });
+    });
+}).catch((err) => {
+    console.error(err);
+});
